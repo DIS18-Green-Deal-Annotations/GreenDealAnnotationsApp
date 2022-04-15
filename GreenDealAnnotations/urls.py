be204ee663176be4c86d3d2e70d9511f7cf122db
okubon <oliver.kubon@smail.th-koeln.de>
2022-04-15 22:36:32 +0200

    added page routing for timeline

--- GreenDealAnnotations/urls.py
+++ GreenDealAnnotations/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.views.generic import TemplateView
 from apps.core.views import index
 
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
     path('', index),
+    path('timeline/', include('apps.date_extraction.urls')),
 ]
--- apps/date_extraction/views.py
+++ apps/date_extraction/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+# only to render the page
+def index(request):
+    return render(request, 'timeline.html')
