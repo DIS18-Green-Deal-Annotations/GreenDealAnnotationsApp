6413351cfcacf0f79fce744c5be154ce5add53da
okubon <oliver.kubon@smail.th-koeln.de>
2022-04-15 22:37:42 +0200

    added new modell for date extraction

--- apps/date_extraction/models.py
+++ apps/date_extraction/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-from ..core.models import Documents
 
 # Create your models here.
 
-class Date(models.Models):
-    document = models.ForeignKey(
-        Documents,
-        on_delete=models.CASCADE,
-    )
+class DateExtraction(models.Model):
+    DocName = models.CharField(max_length=50)
+    DocSentence = models.TextField()
+    DateLabel = models.CharField(max_length=10) 
+    IsoDate = models.CharField(max_length=10) # do we need a string or a date?
