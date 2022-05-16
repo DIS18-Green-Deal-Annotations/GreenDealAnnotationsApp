e1c157dc8fc622ea1de8c25bca26876ff891dcee
xxjuly <janina.blum@smail.th-koeln.de>
2022-05-16 14:27:04 +0200

    implementation context, pseudo filter boxes

--- apps/date_extraction/views.py
+++ apps/date_extraction/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-
+from .models import DateExtraction
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
 # only to render the page
+@csrf_exempt
 def index(request):
-    return render(request, 'timeline.html')
+
+    data = DateExtraction.objects.all()
+    data_object = DateExtraction.objects.all().values() 
+
+    distinct_doc_names = DateExtraction.objects.all().values_list('DocName', flat=True).distinct().order_by("DocName") # distinct values
+    # distinct_doc_names = DateExtraction.objects.all().values('DocName').distinct() # distinct objects 
+    # {'DocName': 'Social Climate Fund'}
+    # {'DocName': 'Proposal for a Directive on energy efficiency (rec'}
+
+    distinct_years = DateExtraction.objects.all().values_list('IsoDate', flat=True).distinct().order_by("IsoDate")
+
+    context = {
+        "data": data, 
+        "doc_names_filter": distinct_doc_names,
+        "year_filter": distinct_years,
+        "filter_data" : data_object,
+    }
+
+    return render(request, 'timeline.html', context)
