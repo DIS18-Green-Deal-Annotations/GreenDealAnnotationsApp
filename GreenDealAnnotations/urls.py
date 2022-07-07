6770ec6b30cdf03a71ceb178dac85569ed5783db
Paul Nebatz <paul.nebatz@outlook.com>
2022-07-07 23:24:14 +0200

    updated urls to match pattern

--- GreenDealAnnotations/urls.py
+++ GreenDealAnnotations/urls.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
 from core.views import index
-from core.views import DocumentViewer, timeline
+from core.views import DocumentViewer, timeline, browse_documents
 
 urlpatterns = [
     # pages by groups
-    path('timeline/', timeline),
-    path('documentviewer/<str:id>/', DocumentViewer),
+    path('timeline/', timeline, name='timeline'),
+    path('documentviewer/<str:id>/', DocumentViewer, name='document_details'),
+    path('documentviewer/', browse_documents, name='all_documents'),
 
     # general
     path('admin/', admin.site.urls),
